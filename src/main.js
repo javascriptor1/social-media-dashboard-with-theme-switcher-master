da7f407eff48f5549ff49ccb0ed51bdfb65615a7
Mohammed Fakih <mhd.fakih@gmail.com>
2023-12-02 19:47:52 +0300

    youtube bg white fix for last div

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const rootEl = document.documentElement; // get root document element
 const toggleEl = document.getElementById("checkbox");
 const instagramDiv = document.querySelector(".instagram");
 const userTheme = localStorage.getItem("theme"); // to save settings in local storage
+const youTubeIconDiv = document.querySelector(".span-yt");
 
 const themeChecker = () => {
   if (userTheme === "dark") {
@@ -17,9 +18,11 @@ function themeSwitcher() {
     rootEl.classList.remove("dark");
     instagramDiv.classList.remove("border-fix");
     localStorage.setItem("theme", "light");
+    youTubeIconDiv.classList.remove("youtube");
     return;
   }
   rootEl.classList.add("dark");
+  youTubeIconDiv.classList.add("youtube");
   localStorage.setItem("theme", "dark");
   instagramDiv.classList.add("border-fix");
   localStorage.setItem("theme", "dark");
