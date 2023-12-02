5c8f23d86096c9104c244a54746dd68085f612dc
Mohammed Fakih <mhd.fakih@gmail.com>
2023-12-02 14:15:05 +0300

    javascript toggle switch for dark mode done

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
-'use strict'
+"use strict";
 
+const rootEl = document.getElementsByTagName("html");
+console.log(rootEl);
+const toggleEl = document.getElementById("checkbox");
+const instagramDiv = document.querySelector(".instagram");
+
+function themeSwitcher() {
+  rootEl[0].classList.toggle("dark");
+  instagramDiv.classList.toggle("border-fix");
+}
+
+toggleEl.addEventListener("change", themeSwitcher);
