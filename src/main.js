806d69aad1590d70350416a0f204d758dacf2e1f
Mohammed Fakih <mhd.fakih@gmail.com>
2023-12-02 15:08:04 +0300

    js with localstorage done

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
 "use strict";
 
-const rootEl = document.getElementsByTagName("html");
-console.log(rootEl);
+const rootEl = document.documentElement; // get root document element
 const toggleEl = document.getElementById("checkbox");
 const instagramDiv = document.querySelector(".instagram");
+const userTheme = localStorage.getItem("theme"); // to save settings in local storage
+
+const themeChecker = () => {
+  if (userTheme === "dark") {
+    rootEl.classList.add("dark");
+    toggleEl.checked = true;
+  }
+};
 
 function themeSwitcher() {
-  rootEl[0].classList.toggle("dark");
+  if (rootEl.classList.contains("dark")) {
+    rootEl.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    return;
+  }
+  rootEl.classList.add("dark");
+  localStorage.setItem("theme", "dark");
   instagramDiv.classList.toggle("border-fix");
+  localStorage.setItem("theme", "dark");
 }
 
 toggleEl.addEventListener("change", themeSwitcher);
+
+themeChecker();
